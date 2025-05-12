3f6679316ad3c4f34c82b0cc993350fb4eb94d37
Sarin Alexander <execode02@gmail.com>
2025-05-12 17:44:22 +0530

    [FEAT] Update Angular project configuration for single-spa integration
    - Changed the build and serve builders to use custom webpack.
    - Updated main entry point to `main.single-spa.ts`.
    - Added custom webpack configuration options in `angular.json`.
    - Updated dependencies in `package.json` and `package-lock.json` for single-spa and custom webpack builders.
    - Modified `app.config.ts` to include single-spa providers.
    - Updated `index.html` title and removed placeholder content from `app.component.html`.
    - Deleted unused `main.ts` file.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Configforge
+# configforge
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.11.
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { getSingleSpaExtraProviders } from 'single-spa-angular';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
+  providers: [
+    getSingleSpaExtraProviders(),
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/configforge' },
+  ],
+}
