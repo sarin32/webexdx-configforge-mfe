fd02ca4fe1a03ad8c5634b6093d75a82f461d575
Sarin Alexander <execode02@gmail.com>
2025-08-30 14:51:08 +0530

    [FEAT] Add Dockerfile for development setup and configure webpack for single-spa

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,5 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
-
-  // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
 };
