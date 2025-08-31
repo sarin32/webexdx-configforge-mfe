abc5f058722b3758955cca1b802c6ad51feeb21f
Sarin Alexander <execode02@gmail.com>
2025-08-31 23:44:29 +0530

    [FEAT] Replace button with a centered "Work In Progress" message in ProjectListComponent

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { HlmButton } from '@spartan-ng/helm/button';
 @Component({
   selector: 'app-project-list',
-  imports: [],
+  imports: [HlmButton],
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.css'
 })
